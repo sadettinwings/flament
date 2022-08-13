45d2768f4596647c8bb1bd5883ba7e19f24e18a7
sadettinwings <sadettin@wings.com.tr>
2022-08-13 19:42:29 +0300

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Support\Facades\Schema;
+
 
 use Illuminate\Support\ServiceProvider;
 
@@ -22,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        
+        Schema::defaultStringLength(191);
+
     }
 }
